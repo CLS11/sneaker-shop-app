5fe80804ddf39890e01e8e3f0ab5aa0416937165
crystall.101600 <crystall.101600@gmail.com>
2024-09-03 13:41:07 +0000

    It is the main cart page displaying all the items in the cart.

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/cart.dart';
+import 'package:myapp/models/shoe.dart';
+import 'package:provider/provider.dart';
+import 'package:myapp/components/cart_item.dart';
 
-
-class CartPage extends StatefulWidget {
+class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cart'),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //heading
+            Text(
+              'MY CART',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  //getting the shoe
+                  Shoe individualShoe = value.userCart[index];
+
+                  //returning the cart item
+                  return CartItem(shoe: individualShoe);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
