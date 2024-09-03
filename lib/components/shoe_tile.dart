a9def46884d2f316f1e574c26bef162233280571
crystall.101600 <crystall.101600@gmail.com>
2024-09-03 13:36:37 +0000

    Created for viewing all the items along with a gesture detector in it.

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/models/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  void Function()? onTap;
+  ShoeTile({super.key, required this.shoe, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // shoe pictures
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.asset(shoe.imagePath),
+          ),
+
+          // description
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              shoe.description,
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+
+          // Details and price
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //shoe name
+                    Text(
+                      shoe.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 10,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    //price
+                    Text(
+                      '\$' + shoe.price,
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+
+                //add button
+                GestureDetector(
+                  onTap: onTap,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      ),
+                    ),
+                    child: const Icon(Icons.add, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          //button to add the cart
+        ],
+      ),
+    );
+  }
+}
